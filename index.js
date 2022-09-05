a5cb16b21f972891583c1d2ff44af851fdf0263d
Achref <medachrefg@gmail.com>
2022-09-05 18:04:58 +0100

    making some changing on backend

--- index.js
+++ index.js
@@ -10,25 +10,34 @@ const app = express();
 
 app.use(cors());
 
-PORT = process.env.PORT;
-const fetch = async () => {
-	try {
-		const response = await axios(process.env.URL);
-		const data = await response.data;
-		const $ = cheerio.load(data);
-		const articules = [];
-		$('.fc-sublink__title', data).each(function () {
-			const title = $(this).text();
-			const url = $(this).find('a').attr('href');
-			articules.push({
-				title,
-				url,
+const PORT = process.env.PORT;
+const URL = process.env.URL;
+
+app.get('/', (req, res, next) => {
+	res.json('this my web Scraper');
+});
+
+app.get('/results', (req, res, next) => {
+	const fetch = async () => {
+		try {
+			const response = await axios(URL);
+			const data = await response.data;
+			const $ = cheerio.load(data);
+			const articules = [];
+			$('.fc-sublink__title', data).each(function () {
+				const title = $(this).text();
+				const url = $(this).find('a').attr('href');
+				articules.push({
+					title,
+					url,
+				});
 			});
-		});
-		console.log(articules);
-	} catch (error) {
-		console.log(error);
-	}
-};
-fetch();
+			res.json(articules);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	fetch();
+});
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`.yellow));
