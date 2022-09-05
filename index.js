2b79d0b27e7b76f6c72ac2b90c0034d60d20b12d
Achref <medachrefg@gmail.com>
2022-09-05 17:46:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const cors = require('cors');
+const colors = require('colors');
+
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+
+PORT = process.env.PORT;
+const fetch = async () => {
+	try {
+		const response = await axios(process.env.URL);
+		const data = await response.data;
+		const $ = cheerio.load(data);
+		const articules = [];
+		$('.fc-sublink__title', data).each(function () {
+			const title = $(this).text();
+			const url = $(this).find('a').attr('href');
+			articules.push({
+				title,
+				url,
+			});
+		});
+		console.log(articules);
+	} catch (error) {
+		console.log(error);
+	}
+};
+fetch();
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
